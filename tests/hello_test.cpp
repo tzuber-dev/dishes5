d92ae16ab7e3cdff1f0730365af3c62ee0da2b2b
tzuber-dev <zt50481@mail.harpercollege.edu>
2026-05-02 21:55:18 -0500

    Final submission for queue.

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -8,6 +8,41 @@
 
 
 
+TEST_CASE("queue has some sort of objects during enqueue execution") {
+    QueueTees waitinglist;
+    Cutie* cutiesobject = nullptr;
+    waitinglist.enqueue(cutiesobject);
+
+    REQUIRE(waitinglist.notfullsize() == false);
+};
+
+
+
+TEST_CASE("enqueue works even after initiating dequeue method") {
+    QueueTees waitinglist;
+    Cutie* cutiesobject = nullptr;
+
+
+
+    for (int x=0; x<10; x++) {
+        waitinglist.enqueue(cutiesobject);
+    };
+
+
+    waitinglist.dequeue();
+
+
+    REQUIRE(waitinglist.size() ==9);
+    waitinglist.enqueue(cutiesobject);
+    REQUIRE(waitinglist.size() ==10);
+
+
+
+};
+
+
+
+
 TEST_CASE( "allowedspace works" ) {
 QueueTees waitinglist;
     Cutie* cutiesobject = nullptr;
@@ -26,4 +61,18 @@ QueueTees waitinglist;
 
 
 
-}
+};
+
+
+TEST_CASE("empty queue does not break program but returns nullptr") {
+    QueueTees waitinglist;
+    Cutie* var1 = waitinglist.dequeue();
+    REQUIRE(waitinglist.size() == 0);
+
+
+
+
+    REQUIRE(var1 == nullptr);
+
+};
+
